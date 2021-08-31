d133c5d2756dc508e90f4fde45989db61cade257
chu-young <ttheko101@gmail.com>
2021-08-31 10:46:24 +0200

    initial build complete

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import styles from './About.module.css'
-
+import icons from './assets-icons/Icons.js'
 
 function About(){
 return(
@@ -10,30 +10,90 @@ return(
 
     <div className={styles.aboutGrid}>
         <div>
+        <h1>Brief Rundown:</h1>
         <div className={styles.information}>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti amet laborum vitae! Consequuntur voluptatem aliquam quae reprehenderit vero rerum minus at facere voluptates saepe, impedit incidunt ab mollitia vel error temporibus in dolores voluptate ut nesciunt aperiam ratione maxime. Repellendus, omnis cum excepturi, illo hic quibusdam fuga similique dolore perspiciatis, quos ipsum quod. Deserunt accusamus, minus aspernatur facilis esse dolor.</p>
+            <p>I am a Front End Developer with an eye for creativity. I enjoy writing code, developing web apps/pages and learning new
+technologies/languages. I am also progress-driven, a
+quick learner, and highly collaborative.</p>
+
+<p>I am also more than happy to relocate anywhere in the world as this would give me the oppotunity to work with new and different people, experience different cultures and explore different countries and terrains, while learning the newest techologies and industry standards.</p>
+
+<p>In my spare time I love to play with my dogs, game and spend time with my friends.</p>
         </div>
         </div>
        
        <div>
-           <h1>Tech</h1>
+           <h1>Tech and Tools I use:</h1>
+           {/*First Row*/}
        <div className={styles.iconGrid}>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
-            <p>Lorem, ipsum.</p>
+            <div className={styles.icon}>
+                <img src={icons.html5} alt="HTML 5" />
+                <span>HTML 5</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.css3} alt="CSS 3" />
+                <span>CSS 3</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.bootstrap} alt="Bootstrap" />
+                <span>Bootstrap</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.materialize} alt="Materialize" />
+                <span>Materialize</span>
+            </div>
+                    {/*Second Row*/}
+            <div className={styles.icon}>
+                <img src={icons.js} alt="HTML 5" />
+                <span>JavaScript/ES6</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.jquery} alt="CSS 3" />
+                <span>JQuery</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.react} alt="React" />
+                <span>React</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.typescript} alt="TypeScript" />
+                <span>TypeScript</span>
+            </div>
+                {/*Third Row*/}
+                <div className={styles.icon}>
+                <img src={icons.wordpress} alt="WordPress" />
+                <span>WordPress</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.figma} alt="Figma" />
+                <span>Figma</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.webpack} alt="Webpack" />
+                <span>Webpack</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.sass} alt="SASS" />
+                <span>SASS</span>
+            </div>
+           
+                    {/*Forth Row*/}
+            <div className={styles.icon}>
+                <img src={icons.npm} alt="NPM" />
+                <span>NPM</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.vscode} alt="VS Code" />
+                <span>VS Code</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.git} alt="Git/Version Control" />
+                <span>Git/Version Control</span>
+            </div>
+            <div className={styles.icon}>
+                <img src={icons.github} alt="Github" />
+                <span>GitHub</span>
+            </div>
         </div>
        </div>
        
